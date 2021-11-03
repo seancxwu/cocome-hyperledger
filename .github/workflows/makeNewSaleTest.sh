3ebed6116210a9aa7a2449f27eb045d8c7b2620b
gqqnbig <gqqnb2005@gmail.com>
2021-11-03 20:46:00 +0800

    Add FABRIC_CFG_PATH otherwise it has error Config File "core" Not Found

--- .github/workflows/makeNewSaleTest.sh
+++ .github/workflows/makeNewSaleTest.sh
@@ -4,6 +4,7 @@
 ./network.sh up createChannel
 ./network.sh deployCC -ccn cocome -ccp $GITHUB_WORKSPACE -ccl java
 
+export FABRIC_CFG_PATH=$PWD/../config/
 source $GITHUB_WORKSPACE/.github/workflows/as-org1.sh
 
 pci -C mychannel -n cocome --waitForEvent -c '{"function":"ManageStoreCRUDServiceImpl:createStore","Args":["1","Target","Weyburn","false"]}'
