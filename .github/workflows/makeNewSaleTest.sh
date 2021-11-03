6a1366c8dc78b2d0f9b3db513a81300043e3cef7
gqqnbig <gqqnb2005@gmail.com>
2021-11-03 20:18:21 +0800

    Use GitHub Actions to run tests

---
+++ .github/workflows/makeNewSaleTest.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+./network.sh down
+./network.sh up createChannel
+./network.sh deployCC -ccn cocome -ccp $GITHUB_WORKSPACE -ccl java
+
+source $GITHUB_WORKSPACE/.github/workflows/as-org1.sh
+
+pci -C mychannel -n cocome --waitForEvent -c '{"function":"ManageStoreCRUDServiceImpl:createStore","Args":["1","Target","Weyburn","false"]}'
+pci -C mychannel -n cocome --waitForEvent -c '{"function":"ManageCashDeskCRUDServiceImpl:createCashDesk","Args":["1","1","false"]}'
+pci -C mychannel -n cocome --waitForEvent -c '{"function":"CoCoMESystemImpl:openStore","Args":["1"]}'
+pci -C mychannel -n cocome --waitForEvent -c '{"function":"CoCoMESystemImpl:openCashDesk","Args":["1"]}'
+
+docker stop $(docker ps -n 1 --filter 'name=dev' --format '{{.ID}}')
+
+pci -C mychannel -n cocome --waitForEvent -c '{"function":"ProcessSaleServiceImpl:makeNewSale","Args":[]}'
+
+
